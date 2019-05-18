4a85e9f2b3d9fd7b30542ce4d31e03cd1a6c579d
João Vitor Barbosa <joaoassisb@gmail.com>
2019-05-17 22:58:06 -0300

    remove pasta build de gitignore

---
+++ frontend/build/precache-manifest.e19c123cdc610bc1eae8c998c67b6121.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81b3c15867446778f143",
+    "url": "/static/js/main.33d003d9.chunk.js"
+  },
+  {
+    "revision": "3c480ce9a059a9a46ca0",
+    "url": "/static/js/2.89e5797f.chunk.js"
+  },
+  {
+    "revision": "81b3c15867446778f143",
+    "url": "/static/css/main.1fadd9ac.chunk.css"
+  },
+  {
+    "revision": "3c480ce9a059a9a46ca0",
+    "url": "/static/css/2.2b3958ab.chunk.css"
+  },
+  {
+    "revision": "230dfbe5844be444239ec87846493bd4",
+    "url": "/index.html"
+  }
+];
